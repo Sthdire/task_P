34198a11ebf12990b17e33542c14e09a8d349fa2
Settter <egor-udon@mail.ru>
2022-08-27 14:36:10 +0300

    update starting

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import db_connect
 from api import api_bp
 from init import app
 
 app.register_blueprint(api_bp, url_prefix='/api')
 
 if __name__ == '__main__':
+    db_connect
     app.run(debug=True)
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-req = requests.delete('http://127.0.0.1:5000/api/delete_message/message=zaebal')
-print(req.text)
